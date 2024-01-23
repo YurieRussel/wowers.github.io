f78e524d28801bf9964375045f5b456b128a3919
YurieRussel <149810481+YurieRussel@users.noreply.github.com>
2024-01-23 19:33:00 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+const sound = document.getElementById("soundEffect")
+
+let id = null
+const element = document.getElementById("heading-Text")
+let pos = 0
+clearInterval(id)
+id = setInterval(frame, 5)
+function frame() 
+{
+    if (pos == 450) 
+    {
+      clearInterval(id);
+
+      //for logo  
+      let id1 = null
+      const element1 = document.getElementById("animiAdv")
+      let pos1 = 0
+      clearInterval(id1)
+      id1 = setInterval(frame1, 10)
+      
+      function frame1()
+      {
+          if (pos1 == 350) 
+          {
+            clearInterval(id1);
+            sound.autoplay = true;
+          } 
+          else 
+          {
+            //position animation
+            pos1++;  
+            element1.style.left = pos1 + "px"
+          }
+      }
+      
+    } 
+    else 
+    {
+      pos++;  
+      element.style.left = pos + "px"
+    }
+}
+
+document.addEventListener('click', musicPlay);
+function musicPlay() {
+    document.getElementById('soundEffect').play();
+    document.getElementById('backgroundMusic').play();
+    document.removeEventListener('click', musicPlay);
+
+}
